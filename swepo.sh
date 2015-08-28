b47868dd29ceff7d23aa8c8838621d2ca8cd3ca6
Rolando Urquiza <rolurquiza@gmail.com>
2015-08-27 21:36:25 -0400

    added source switch

--- swepo.sh
+++ swepo.sh
@@ -5,3 +5,29 @@ if [ $(whoami) != "root" ]; then
     exit
 fi
 
+SOURCES="/etc/apt"
+
+if [ ! -d $SOURCES/sources.list.d ]; then
+    mkdir $SOURCES/sources.list.d
+    echo "put your alternative sources in /etc/apt/sources.list.d"
+    exit 1
+fi
+
+if [ -a $SOURCES/sources.list ]; then
+    mv $SOURCES/sources.list $SOURCES/sources.list.disabled
+fi
+
+# disable all sources list
+shopt -s nullglob
+for source in $SOURCES/sources.list.d/*.list; do
+    mv "$source" "${source}.disabled"
+done
+
+for name; do
+    mv "$SOURCES/sources.list.d/$name.list.disabled" \
+       "$SOURCES/sources.list.d/$name.list"  # enable the desired sources
+done
+
+if [ -z "$@" ]; then
+    mv $SOURCES/sources.list.disabled $SOURCES/sources.list
+fi
