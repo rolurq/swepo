a5591fa76943fcbb55bdb148d92f7507eb0bc43a
Rolando Urquiza <rolurquiza@gmail.com>
2015-12-24 19:06:48 -0500

    Add argument check for edit case

--- swepo.sh
+++ swepo.sh
@@ -21,6 +21,26 @@ if [ $# == 0 ]; then
     mv "$SOURCES/sources.list.disabled" "$SOURCES/sources.list"
 fi
 
+ACTION=enable
+
+while true ; do
+    case "$1" in
+        # set the state acording to the current option
+        -c|--config) ACTION=config ; shift ;;
+        "") break ;;  # no more arguments
+        *)
+            case $ACTION in
+                config)
+                    edit $SOURCES/sources.list.d/$1.list* ;
+                    if [ $? != 0 ] ; then
+                        echo "Error editing!" ; exit 1 ; fi ;
+                    shift ;;
+                *) break ;;
+            esac
+            if [ $? != 0 ] ; then break ; fi ;;
+    esac
+done
+
 # disable all sources list
 shopt -s nullglob
 for source in $SOURCES/sources.list.d/*.list; do
