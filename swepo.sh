9549a5b3e5790f8930c5ed7f7ed800f2c687f052
Rolando Urquiza <rolurquiza@gmail.com>
2015-12-24 19:05:36 -0500

    Fix case of no arguments passed

--- swepo.sh
+++ swepo.sh
@@ -17,6 +17,10 @@ if [ -a $SOURCES/sources.list ]; then
     mv $SOURCES/sources.list $SOURCES/sources.list.disabled
 fi
 
+if [ $# == 0 ]; then
+    mv "$SOURCES/sources.list.disabled" "$SOURCES/sources.list"
+fi
+
 # disable all sources list
 shopt -s nullglob
 for source in $SOURCES/sources.list.d/*.list; do
@@ -27,7 +31,3 @@ for name; do
     mv "$SOURCES/sources.list.d/$name.list.disabled" \
        "$SOURCES/sources.list.d/$name.list"  # enable the desired sources
 done
-
-if [ -z '$@' ]; then
-    mv "$SOURCES/sources.list.disabled" "$SOURCES/sources.list"
-fi
