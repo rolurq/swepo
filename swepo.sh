91df1f2070fd27569c34495d7afab7e3477412f1
Rolando Urquiza <rolurquiza@gmail.com>
2016-05-02 17:42:55 +0000

    Add help parameter

--- swepo.sh
+++ swepo.sh
@@ -9,15 +9,28 @@ if [ $(whoami) != "root" ]; then
     exit
 fi
 
-SOURCES="/etc/apt"
+help() {
+  cat << EOF
+usage: $0 [[ACTION] [LIST1 LIST2 ...]...]
 
-if [ ! -d $SOURCES/sources.list.d ]; then
-    mkdir $SOURCES/sources.list.d
-    echo "put your alternative sources in /etc/apt/sources.list.d"
-    exit 1
-fi
+Each action preceding the list file names will be executed over those
+files until another action its specified.
 
+Actions:
+  -a, --add      Create new list files
+  -c, --config   Edit specified list files
+  -d, --disable  Disable list files
+  -e, --enable   Enable list files
+  -r, --remove   Remove list files
+  -t, --toggle   Toggle disabled/enabled to $SOURCES/sources.list.
+  This action does not interact with list names
 
+    --help         Show this message
+
+Calling without parameters will show tha available list files ad their
+states
+EOF
+}
 
 declare -A LIST_FILES
 for source in `ls $SOURCES/*.list* | cut -d / -f 5 | cut -d . -f 1`; do
@@ -50,6 +63,7 @@ while true ; do
            $SOURCES/sources.list;
       fi
       shift ;;
+    -h|--help) help ; exit 0 ;;
         "") break ;;  # no more arguments
         *)
             # control opposite actions
